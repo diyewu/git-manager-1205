63728278201636a26444e9bbd243102b2bed62ee
diyewu <930725713@qq.com>
2018-04-10 09:04:56 +0800

    0410

--- src/test/java/wzh/Http/MiniProHttpTest.java
+++ src/test/java/wzh/Http/MiniProHttpTest.java
@@ -13,7 +13,7 @@ public class MiniProHttpTest {
 	
 	public static void login() throws UnsupportedEncodingException{
 //		String url = "https://zhonglestudio.cn/gismgr/mini/login";
-		String url = "http://localhost:8095/gismgr/mini/login";
+		String url = "https://zhonglestudio.cn/gismgr/mini/login";
 		
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("userName", "admin");
@@ -24,13 +24,13 @@ public class MiniProHttpTest {
 	}
 	public static void getMapInfoByuserRole() throws UnsupportedEncodingException{
 //		String url = "https://zhonglestudio.cn/gismgr/mini/login";
-		String url = "http://localhost:8095/gismgr/mini/getMapInfoByuserRole";
+		String url = "https://zhonglestudio.cn/gismgr/mini/getMapInfoByuserRole";
 		
 		Map<String, Object> params = new HashMap<String, Object>();
 //		params.put("userName", "admin");
 //		params.put("userPwd", "admin");
 		params.put("randomkey", "011KYMn91e3U4Q1Vq4o91bxCn91KYMnC");
-		params.put("token", "61afc720a9264094a1d2c5a309070aa7");
+		params.put("token", "6fe7d73f8f844b5a9c7b62daceda308e");
 		String resp = HttpUtil.httpPostRequest(url, params);
 		System.out.println(resp);
 	}
